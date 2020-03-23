504964acbddadb8d8a5162b3c64663dfc5a6d3ca
Your name hereJMACBigMan <jemclaurin@gmail.com>
2020-03-23 19:10:50 -0400

    Initial commit

---
+++ src/Javaarraycom/company/Main.java
@@ -0,0 +1,8 @@
+package Javaarraycom.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is James McLaurin and I'm learning to code.");
+    }
+}
